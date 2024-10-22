41c237d13f1bba8652a50c8984b97f6a885d4b76
zzzam21 <jonas01311@gmail.com>
2024-10-21 19:34:03 -0500

    feat: Footer

---
+++ Back_box/src/app/components/pie/pie.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-pie',
+  standalone: true,
+  imports: [ButtonModule],
+  templateUrl: './pie.component.html',
+  styleUrl: './pie.component.css'
+})
+export class PieComponent {
+
+}
