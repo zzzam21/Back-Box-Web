d25a40c1c251653551393fded19c56738a8b3c3d
DanielTrujillo5 <danieltigo555@gmail.com>
2024-10-23 18:09:18 -0500

    feat: comentarios

--- Back_box/src/app/views/comments/comments.component.ts
+++ Back_box/src/app/views/comments/comments.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { EncabezadoComponent } from "../../components/encabezado/encabezado.component";
 import { PieComponent } from "../../components/pie/pie.component";
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [EncabezadoComponent, PieComponent],
+  imports: [EncabezadoComponent, PieComponent, InputTextareaModule, FloatLabelModule, FormsModule,ButtonModule],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.css'
 })
