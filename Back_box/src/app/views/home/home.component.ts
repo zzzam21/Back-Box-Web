4363cde7f1a18acd46983b6e88ba35fdeb054fa2
zzzam21 <jonas01311@gmail.com>
2024-10-24 17:24:16 -0500

    feat: avance Home

--- Back_box/src/app/views/home/home.component.ts
+++ Back_box/src/app/views/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
+import { CardModule } from 'primeng/card';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule,ButtonModule,TagModule,CardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  responsiveOptions: any[] | undefined;
+  user: any[] = [
+    {name:'Raffaele Forte',description:'Fundador de Back Box y <br> desarrollador principal'},
+    {name:'Stefan Umit Uygur',description:'Encargada de las relaciones publicas'},
+    {name:'Gianfranco Costamagna',description:'Encargado de mantener el repositorio'},
+    {name:'Andrea Draghetti',description:'Community Staff'}
+  ];
 }
