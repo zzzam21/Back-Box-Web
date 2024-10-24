c973bc0dd1045f8be2bc108a5ebe75bf888c66b9
Alexander Yama <alexyr24@gmail.com>
2024-10-24 17:16:41 -0500

    feat: configurationfinal

--- Back_box/src/app/views/configuration/configuration.component.ts
+++ Back_box/src/app/views/configuration/configuration.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { SplitterModule } from 'primeng/splitter';
 import { CardModule } from 'primeng/card';
 import { TabViewModule } from 'primeng/tabview';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-configuration',
   standalone: true,
-  imports: [SplitterModule,CardModule,TabViewModule],
+  imports: [SplitterModule,CardModule,TabViewModule,DividerModule],
   templateUrl: './configuration.component.html',
   styleUrl: './configuration.component.css'
 })
 export class ConfigurationComponent {
-
+  sidebarVisible4: boolean = false;
 }
+
