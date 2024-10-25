c4deb0e0e28afc1dcf7cb798fdabd2e1f9f69cc1
zzzam21 <jonas01311@gmail.com>
2024-10-25 12:52:03 -0500

    feat: finalizado siteInteresting

--- Back_box/src/app/views/intesting/intesting.component.ts
+++ Back_box/src/app/views/intesting/intesting.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ImageModule } from 'primeng/image';
 @Component({
   selector: 'app-intesting',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule,CardModule,ImageModule],
   templateUrl: './intesting.component.html',
   styleUrl: './intesting.component.css'
 })
