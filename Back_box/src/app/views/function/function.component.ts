115205cea2244b8af2e6d6a516bf30864c9cef0a
Alexander Yama <alexyr24@gmail.com>
2024-10-27 22:10:59 -0500

    feat: function1.0

--- Back_box/src/app/views/function/function.component.ts
+++ Back_box/src/app/views/function/function.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-function',
   standalone: true,
-  imports: [],
+  imports: [CardModule],
   templateUrl: './function.component.html',
   styleUrl: './function.component.css'
 })
