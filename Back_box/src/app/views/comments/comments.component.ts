53e50d504376061c205ad85224dca8a5b74958d7
zzzam21 <jonas01311@gmail.com>
2024-10-28 22:13:40 -0500

    feat: cambio

--- Back_box/src/app/views/comments/comments.component.ts
+++ Back_box/src/app/views/comments/comments.component.ts
@@ -6,11 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [EncabezadoComponent, PieComponent, InputTextareaModule, FloatLabelModule, FormsModule,ButtonModule, CardModule],
+  imports: [ImageModule,EncabezadoComponent, PieComponent, InputTextareaModule, FloatLabelModule, FormsModule,ButtonModule, CardModule],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.css'
 })
