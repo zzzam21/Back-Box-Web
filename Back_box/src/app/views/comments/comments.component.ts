32812f8162bd1ec877f2b7b5de195e400d56ac5a
zzzam21 <jonas01311@gmail.com>
2024-10-29 22:21:10 -0500

    feat: Funciona Comentarios con API FormSubmit

--- Back_box/src/app/views/comments/comments.component.ts
+++ Back_box/src/app/views/comments/comments.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { EncabezadoComponent } from "../../components/encabezado/encabezado.component";
 import { PieComponent } from "../../components/pie/pie.component";
 import { InputTextareaModule } from 'primeng/inputtextarea';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
@@ -11,7 +11,7 @@ import { ImageModule } from 'primeng/image';
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [ImageModule,EncabezadoComponent, PieComponent, InputTextareaModule, FloatLabelModule, FormsModule,ButtonModule, CardModule],
+  imports: [ImageModule,EncabezadoComponent, PieComponent, InputTextareaModule, FloatLabelModule,ButtonModule, CardModule],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.css'
 })
